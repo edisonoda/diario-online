a1459686c43d440a981983d1a1b7a6acf6599d0c
Edison Furusato <edison.oda@inteliger.com.br>
2023-08-15 00:26:29 -0300

    Não sobrescrevendo os headers da req

--- src/app/core/interceptors/header/header.service.ts
+++ src/app/core/interceptors/header/header.service.ts
@@ -13,8 +13,13 @@ export class HeaderInterceptor implements HttpInterceptor {
   constructor(private headerService: HeaderService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const headers = this.headerService.headers;
+
+    if (request.headers)
+      Object.entries(request.headers).forEach(([key, value]) => headers.append(key, value));
+
     return next.handle(request.clone({
-      headers: this.headerService.headers
+      headers: headers
     }));
   }
 }
