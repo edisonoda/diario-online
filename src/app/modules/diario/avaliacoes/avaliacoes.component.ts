0b4ae5999b06c873f93652f577786099c24e79f2
Edison Furusato <edison.oda@inteliger.com.br>
2023-08-15 19:45:56 -0300

    Add início da tab de avaliações do diário

--- src/app/modules/diario/avaliacoes/avaliacoes.component.ts
+++ src/app/modules/diario/avaliacoes/avaliacoes.component.ts
@@ -1,5 +1,5 @@
 import { ActivatedRoute, Router } from '@angular/router';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { SessaoService } from 'src/app/core/services/sessao.service';
 import { DiarioService } from '../../../core/services/diario.service';
 
@@ -9,6 +9,19 @@ import { DiarioService } from '../../../core/services/diario.service';
   styleUrls: ['./avaliacoes.component.css'],
 })
 export class AvaliacoesComponent implements OnInit, OnDestroy {
+  @Input() avaliacoes: any[] = [];
+  subAvaliacoes: any[] = [];
+
+  houveModificacao: boolean = false;
+  habilitaBotaoSubDivisao: boolean = false;
+
+  quantTelaMax: number = 0; // Maximo de aulas na tela
+  indiceInicial: number = 0; // Primeira aula visivel na tela
+
+  botaoEsquerda: boolean = false; // Indica se os botoes de navegação estarão habilitados
+  botaoDireita: boolean = false;
+
+  permissoes: string[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -16,9 +29,60 @@ export class AvaliacoesComponent implements OnInit, OnDestroy {
     private diarioService: DiarioService,
     private router: Router
   ) {
+    if (this.sessaoService.permissoes)
+      this.permissoes = JSON.parse(this.sessaoService.permissoes);
   }
 
   ngOnInit() {
+  }
+
+  focarPersistencia(): void {
+
+  }
+
+  trocarDivisao(ev: Event): void {
+
+  }
+
+  gerenciarAvaliacoes(ev: Event): void {
+
+  }
+
+  rolar(direcao: string): void {
+    if (direcao === 'esq') {
+      this.indiceInicial--;
+    } else {
+      this.indiceInicial++;
+    }
+
+    this.calcularBotoesAtivos();
+  };
+
+  obterSubAvaliacoes(): void {
+    this.subAvaliacoes = [];
+    this.avaliacoes.forEach(aval => {
+      aval.subAvaliacoes.forEach((sub: any) => {
+        sub.nomeAvaliacao = aval.nome;
+        this.subAvaliacoes.push(sub);
+      });
+    });
+    
+    this.subAvaliacoes;
+  };
+
+  calcularBotoesAtivos(): void {
+    var totalAvaliacoes = this.avaliacoes !== null && this.avaliacoes !== undefined ? this.subAvaliacoes.length : 0;
+
+    if (totalAvaliacoes <= this.quantTelaMax) { // Se todos estão visiveis, o indice deve ser zero
+      this.indiceInicial = 0;
+    } else { // ao aumentar a janela, tentamos manter o indice, caso não seja grande demais
+      if ((this.indiceInicial + this.quantTelaMax) > totalAvaliacoes) {
+        this.indiceInicial = totalAvaliacoes - this.quantTelaMax;
+      }
+    }
+
+    this.botaoEsquerda = this.indiceInicial > 0;
+    this.botaoDireita = (this.indiceInicial + this.quantTelaMax) < totalAvaliacoes;
   }
 
   ngOnDestroy(): void { }
