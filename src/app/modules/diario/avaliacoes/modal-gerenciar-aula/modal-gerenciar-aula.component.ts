f99ee4b03c4da1688a70d77186f547033b20a71f
Daniel Schultz <daniel.schultz@caed.ufjf.br>
2023-08-16 15:29:16 -0300

    update

--- src/app/modules/diario/avaliacoes/modal-gerenciar-aula/modal-gerenciar-aula.component.ts
+++ src/app/modules/diario/avaliacoes/modal-gerenciar-aula/modal-gerenciar-aula.component.ts
@@ -1,40 +1,98 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-modal-gerenciar-aula',
   templateUrl: './modal-gerenciar-aula.component.html',
   styleUrls: ['./modal-gerenciar-aula.component.scss']
 })
-export class ModalGerenciarAulaComponent implements OnInit, OnDestroy{
+export class ModalGerenciarAulaComponent implements OnInit, OnDestroy {
 
   indiceNotaEditada = -1;
   avaliacoes: any;
   lista: any;
   filtros: any;
-  this.avaliacao = {
+
+  constructor(
+    public dialogRef: MatDialogRef<ModalGerenciarAulaComponent>,
+    @Inject(MAT_DIALOG_DATA) public dados: any,
+    private snackBar: MatSnackBar,
+  ) {
+    this.avaliacoes = dados.avaliacoes;
+    this.lista = dados.lista;
+    this.filtros = dados.filtros;
+  }
+
+  this
+.
+  avaliacao = {
     descricao: null,
     nota: null
   }
-  this.computaConceito = this.filtros.disciplina.tipoMedida === 'CONCEITO';
-  this.qtMaximoSubDivisoes = this.filtros.disciplina.qtMaximoSubavaliacoes;
+  this
+.
+  computaConceito = this.filtros.disciplina.tipoMedida === 'CONCEITO';
+  this
+.
+  qtMaximoSubDivisoes = this.filtros.disciplina.qtMaximoSubavaliacoes;
 
-  this.indexAvaliacaoSelecionada = null;
+  this
+.
+  indexAvaliacaoSelecionada = null;
 
-  this.avaliacoes = [];
-  angular.copy(avaliacoes.filter(function (av:any) {
+  this
+.
+  avaliacoes = [];
+
+  let
+  avaliacoesFilter = this.ava.filter(function (av: any) {
     return av.flAvaliacaoFormativa == false
-  }), this.avaliacoes);
+  })
+  this
+.
+  avaliacoes
+.
 
-  this.lista = lista;
-  this.notaMaximaDivisao = filtros.divisao.notaMaxima;
-  if (this.avaliacoes[this.indexAvaliacaoSelecionada] && this.isTipoCalculoMedia()) {
-  this.avaliacao.nota = this.avaliacoes[this.indexAvaliacaoSelecionada].notaMaxima;
+  push(JSON
+
+.
+
+  parse(JSON
+
+.
+
+  stringify(avaliacoesFilter)
+
+));
+
+  this
+.
+  notaMaximaDivisao = this.filtros.divisao.notaMaxima;
+
+  if(this
+
+.
+  avaliacoes
+  [this.indexAvaliacaoSelecionada]
+&&
+  this
+.
+
+  isTipoCalculoMedia()
+
+) {
+  this
+.
+  avaliacao
+.
+  nota = this.avaliacoes[this.indexAvaliacaoSelecionada].notaMaxima;
 }
 
 this.getSomaAvaliacoes = function () {
   var contador = 0;
   contador += this.avaliacao.nota;
-  this.avaliacoes[this.indexAvaliacaoSelecionada].subAvaliacoes.forEach(function (entry:any, index:any) {
+  this.avaliacoes[this.indexAvaliacaoSelecionada].subAvaliacoes.forEach(function (entry: any, index: any) {
     if (this != index && entry.flagSubTipo && !entry.isRecuperacao) {
       contador += entry.notaMaxima;
     }
@@ -43,7 +101,7 @@ this.getSomaAvaliacoes = function () {
   return contador;
 }
 
-this.isTipoCalculoMedia = function() {
+this.isTipoCalculoMedia = function () {
   return (this.avaliacoes[this.indexAvaliacaoSelecionada] &&
     this.avaliacoes[this.indexAvaliacaoSelecionada].tipoCalculoSubavaliacoes === 2); // 2 = MEDIA
 }
@@ -59,8 +117,8 @@ this.novaAvaliacao = function () {
 };
 
 var desselecionarItens = function () {
-  this.avaliacoes.forEach(function (aval:any) {
-    aval.subAvaliacoes.forEach(function (entry:any) {
+  this.avaliacoes.forEach(function (aval: any) {
+    aval.subAvaliacoes.forEach(function (entry: any) {
       entry.selecionado = false;
     });
   });
@@ -69,71 +127,71 @@ var desselecionarItens = function () {
 this.salvarAvaliacao = function () {
   var tamanho = null;
   if (this.avaliacoes) {
-    tamanho = this.avaliacoes[this.indexAvaliacaoSelecionada].subAvaliacoes.filter(function (av:any, index:any) {
-      return av.flagSubTipo == true && !(av.selecionado && index == indiceNotaEditada) && av.flAvaliacaoFormativa == false && (av.isRecuperacao == false || av.isRecuperacao === undefined)
+    tamanho = this.avaliacoes[this.indexAvaliacaoSelecionada].subAvaliacoes.filter(function (av, index) {
+      return av.flagSubTipo == true && !(av.selecionado && index == this.indiceNotaEditada) && av.flAvaliacaoFormativa == false && (av.isRecuperacao == false || av.isRecuperacao === undefined)
     }).length;
   }
 
   if (this.qtMaximoSubDivisoes && tamanho == this.qtMaximoSubDivisoes) {
-    .erro('O número de SubTipos de Avaliação não pode ser maior que a quantidade máxima permitida: ' + $scope.qtMaximoSubDivisoes);
-
+    this.snackBar.open('O número de SubTipos de Avaliação não pode ser maior que a quantidade máxima permitida: ' + this.qtMaximoSubDivisoes);
     return;
   }
 
-  if ($scope.avaliacao.descricao === null || $scope.avaliacao.descricao === undefined || $scope.avaliacao.descricao === '') {
-    messagesService.erro('Atenção favor informar a descrição.');
-
+  if (this.avaliacao.descricao === null || this.avaliacao.descricao === undefined || this.avaliacao.descricao === '') {
+    this.snackBar.open('Atenção favor informar a descrição.');
     return;
   }
 
-  if (!$scope.computaConceito) {
-    if ($scope.avaliacao.nota === undefined || $scope.avaliacao.nota === null) {
-      messagesService.erro('Atenção favor informar a nota máxima.');
+  if (!this.computaConceito) {
+    if (this.avaliacao.nota === undefined || this.avaliacao.nota === null) {
+      this.snackBar.open('Atenção favor informar a nota máxima.');
       return;
-    } else if ($scope.avaliacao.nota <= 0) {
-      messagesService.erro('Atenção favor informar uma nota maior que zero.');
-      return;
-    }
-
-    if (!$scope.isTipoCalculoMedia() && $scope.getSomaAvaliacoes() > $scope.avaliacoes[$scope.indexAvaliacaoSelecionada].notaMaxima) {
-      messagesService.erro('Atenção a soma das avaliações deve ser inferior a nota máxima do tipo de avaliação.');
+    } else if (this.avaliacao.nota <= 0) {
+      this.snackBar.open('Atenção favor informar uma nota maior que zero.');
       return;
     }
 
-    if (!$scope.avaliacoes[$scope.indexAvaliacaoSelecionada].flagPermiteSubTipo && $scope.avaliacoes[$scope.indexAvaliacaoSelecionada].temLancamento) {
-      messagesService.erro('Já existem notas lançadas para a avaliação ' + $scope.avaliacoes[$scope.indexAvaliacaoSelecionada].nome + '.');
+    if (!this.isTipoCalculoMedia() && this.getSomaAvaliacoes() > this.avaliacoes[this.indexAvaliacaoSelecionada].notaMaxima) {
+      this.snackBar.open('Atenção a soma das avaliações deve ser inferior a nota máxima do tipo de avaliação.');
+      return;
+    }
+
+    if (!this.avaliacoes[this.indexAvaliacaoSelecionada].flagPermiteSubTipo && this.avaliacoes[this.indexAvaliacaoSelecionada].temLancamento) {
+      this.snackBar.open('Já existem notas lançadas para a avaliação ' + this.avaliacoes[this.indexAvaliacaoSelecionada].nome + '.');
+      ;
       return;
     }
   }
 
-  if (indiceNotaEditada == -1) {
+  if (this.indiceNotaEditada == -1) {
     var programacaoDivisaoNotaRestVO = {
-      nome: $scope.avaliacao.descricao,
-      notaMaxima: $scope.avaliacao.nota,
+      nome: this.avaliacao.descricao,
+      notaMaxima: this.avaliacao.nota,
       flagSubTipo: true,
       selecionado: false,
       isRecuperacao: false,
       flAvaliacaoFormativa: false
     };
-    $scope.avaliacoes[$scope.indexAvaliacaoSelecionada].subAvaliacoes.push(programacaoDivisaoNotaRestVO);
+
+    this.avaliacoes[this.indexAvaliacaoSelecionada].subAvaliacoes.push(programacaoDivisaoNotaRestVO);
   } else {
-    var notaEditada = $scope.avaliacoes[$scope.indexAvaliacaoSelecionada].subAvaliacoes[indiceNotaEditada];
-    notaEditada.nome = $scope.avaliacao.descricao;
-    notaEditada.notaMaxima = $scope.avaliacao.nota;
+    var notaEditada = this.avaliacoes[this.indexAvaliacaoSelecionada].subAvaliacoes[this.indiceNotaEditada];
+    notaEditada.nome = this.avaliacao.descricao;
+    notaEditada.notaMaxima = this.avaliacao.nota;
   }
 
-  $scope.avaliacao.descricao = null;
-  $scope.avaliacao.nota = null;
-  indiceNotaEditada = -1;
+  this.avaliacao.descricao = null;
+  this.avaliacao.nota = null;
+  this.indiceNotaEditada = -1;
   desselecionarItens();
 };
 
-$scope.obterQtdSelecionados = function () {
-  if ($scope.indexAvaliacaoSelecionada == null) {
+this.obterQtdSelecionados = function () {
+  if (this.indexAvaliacaoSelecionada == null) {
     return 0;
   }
 
-  var qtdSelecionados = $scope.avaliacoes[$scope.indexAvaliacaoSelecionada].subAvaliacoes.map(function (entry) {
+  var qtdSelecionados = this.avaliacoes[this.indexAvaliacaoSelecionada].subAvaliacoes.map(function (entry) {
     return entry.selecionado === true ? 1 : 0;
   });
 
@@ -142,51 +200,48 @@ $scope.obterQtdSelecionados = function () {
   });
 };
 
-$scope.selecionarAtividadeNota = function (indice) {
-  $scope.avaliacoes[$scope.indexAvaliacaoSelecionada].subAvaliacoes.forEach(function (entry) {
+this.selecionarAtividadeNota = function (indice: any) {
+  this.avaliacoes[this.indexAvaliacaoSelecionada].subAvaliacoes.forEach(function (entry: any) {
     if (entry.id !== indice) {
       entry.selecionado = false;
     }
   });
 };
 
-$scope.editarAtividadeNota = function () {
-  $scope.avaliacoes[$scope.indexAvaliacaoSelecionada].subAvaliacoes.forEach(function (entry, index) {
+this.editarAtividadeNota = function () {
+  this.avaliacoes[this.indexAvaliacaoSelecionada].subAvaliacoes.forEach(function (entry: any, index: any) {
     if (entry.selecionado) {
-      $scope.avaliacao.descricao = entry.nome;
-      $scope.avaliacao.nota = entry.notaMaxima;
-      indiceNotaEditada = index;
+      this.avaliacao.descricao = entry.nome;
+      this.avaliacao.nota = entry.notaMaxima;
+      this.indiceNotaEditada = index;
     }
   });
 };
 
-$scope.excluir = function () {
-  $scope.avaliacoes[$scope.indexAvaliacaoSelecionada].subAvaliacoes.forEach(function (entry, index) {
+this.excluir = function () {
+  this.avaliacoes[this.indexAvaliacaoSelecionada].subAvaliacoes.forEach(function (entry: any, index: any) {
     if (entry.selecionado) {
-      $scope.avaliacoes[$scope.indexAvaliacaoSelecionada].subAvaliacoes.splice(index, 1);
+      this.avaliacoes[this.indexAvaliacaoSelecionada].subAvaliacoes.splice(index, 1);
     }
   });
-  $scope.avaliacao.descricao = null;
-  $scope.avaliacao.nota = null;
-  indiceNotaEditada = -1;
+  this.avaliacao.descricao = null;
+  this.avaliacao.nota = null;
+  this.indiceNotaEditada = -1;
 };
 
-
-$scope.fecharJanela = function () {
-  $mdDialog.cancel();
+this.fecharJanela = function () {
+  this.dialogRef.close(null);
 };
 
 this.salvar = function () {
-  var somaAvaliacoes = $scope.getSomaAvaliacoes();
+  var somaAvaliacoes = this.getSomaAvaliacoes();
   if (!this.isTipoCalculoMedia() &&
     somaAvaliacoes != null && somaAvaliacoes !== 0 &&
-    somaAvaliacoes !== $scope.avaliacoes[$scope.indexAvaliacaoSelecionada].notaMaxima) {
-    messagesService.erro('A soma dos SubTipos de Avaliação deve ser igual à nota máxima do Tipo de Avaliação selecionado.');
+    somaAvaliacoes !== this.avaliacoes[this.indexAvaliacaoSelecionada].notaMaxima) {
+    this.snackBar.open('A soma dos SubTipos de Avaliação deve ser igual à nota máxima do Tipo de Avaliação selecionado.);
     return;
   } else {
-    $mdDialog.hide($scope.avaliacoes);
+    this.dialogRef.close(null);
   }
-};
 }
 
-}
