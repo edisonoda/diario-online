b808808246c5a9ea905c99af0a060e62cd4118c5
Daniel Schultz <daniel.schultz@caed.ufjf.br>
2023-08-24 10:11:50 -0300

    correcao headers tabela frequencia atualizacao sessionStorage para localStorage

--- src/app/modules/diario/frequencia/botao-chamada/botao-chamada.component.ts
+++ src/app/modules/diario/frequencia/botao-chamada/botao-chamada.component.ts
@@ -30,6 +30,7 @@ export class BotaoChamadaComponent implements OnInit, OnDestroy {
 
   // Configuração do que aparece na tela (letra, cor, estilo)
   configAtual() {
+      console.log(this.aula)
       var letra, descricao;
       if (this.aula.flPresente === null || this.aula.flPresente === undefined) {
         letra = '&nbsp;';
