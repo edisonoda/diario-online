341171fa0b67b9af98b47efa0418072abfb959d2
Edison Furusato <edison.oda@inteliger.com.br>
2023-08-29 19:03:58 -0300

    Ajuste de blur do loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadingService } from './shared/components/loader-geral/loader-geral.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() { }
+export class AppComponent implements OnInit {
+
+  loading: boolean = false;
+
+  constructor(private loadingService: LoadingService) { }
+
+  ngOnInit(): void {
+    this.loadingService.loadingSub
+      .subscribe((loading) => {
+        this.loading = loading;
+      });
+  }
 }
